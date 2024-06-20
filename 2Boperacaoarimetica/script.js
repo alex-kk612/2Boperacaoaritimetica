de917d316e9e6c22f2d678731b77ffe4936a8d96
alex-kk612 <160244686+alex-kk612@users.noreply.github.com>
2024-06-20 08:10:41 -0300

    Carga inicial

---
+++ 2Boperacaoarimetica/script.js
@@ -0,0 +1,34 @@
+//var a = 78;
+//var b = 15;
+//var total = a + b;
+//console.log(total);
+
+//var a = 78;
+//var b = 15
+//var total = a - b
+//console.log(total)
+
+//var a = 78;
+//var b = 15
+//var total = a * b
+//console.log(total)
+
+//var a = 78;
+//var b = 15
+//var total = a / b
+//console.log(total)
+
+//var a = 78;
+//var b = 15
+//var total = a % b
+//console.log(total)
+
+//var a = 78;
+//var b = 15
+//var total = a ++ b
+//console.log(total)
+
+var a = 78;
+var b = 15
+var total = a -- b
+console.log(total)
